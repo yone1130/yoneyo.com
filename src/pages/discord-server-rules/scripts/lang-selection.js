f961e7bc2c4087860bc4b1558e68764348ba36d9
よね/Yone <git@yoneyo.com>
2025-12-06 22:38:11 +0900

    fix(discord-rules): reference to LANGUAGE_TO_FULL_NAMES

--- src/pages/discord-server-rules/scripts/lang-selection.js
+++ src/pages/discord-server-rules/scripts/lang-selection.js
@@ -55,7 +55,7 @@ class LanguageSelector {
             navigator.language || navigator.userLanguage
         ).toLowerCase();
 
-        return LANGUAGE_TO_FULL_NAMES[clientLanguage] || LanguageSelector.DEFAULT_LANGUAGE;
+        return LanguageSelector.LANGUAGE_TO_FULL_NAMES[clientLanguage] || LanguageSelector.DEFAULT_LANGUAGE;
     }
 
     /**
